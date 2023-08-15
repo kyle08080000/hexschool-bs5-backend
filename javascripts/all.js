99f02aa06610a2cd51c63a57b8af65953030dd46
kyle08080000 <s106441109@gmail.com>
2023-08-15 19:02:32 +0800

    add hexschool-bs5-backend

---
+++ javascripts/all.js
@@ -0,0 +1,7 @@
+// sidebar 展開
+const toggleMenuBtn = document.querySelector('#toggle-btn');
+const body = document.querySelector('body');
+toggleMenuBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    body.classList.toggle('sidebar-toggled');
+})
